eebef55f4d11b20ede7c034c3c7eb30172544b83
JezisVedToJeMojzis <mojzissamuel@gmail.com>
2023-12-04 14:09:10 +0100

    fix

--- calculate_delay_traffic_shaper.py
+++ calculate_delay_traffic_shaper.py
@@ -26,13 +26,13 @@ def plot_graph(delays):
     plt.plot(time, delay, marker='o')
     plt.title('Delay Induced by Shaper Over Time')
     plt.xlabel('Time (seconds)')
-    plt.ylabel('Delay')
+    plt.ylabel('Delay (seconds)')
     plt.show()
 
 def main():
     # Define input data
-    time_intervals = [10, 5, 20, 10]  # Adjusted time intervals to cover 45 seconds
-    incoming_rates = [5, 8, 2.5, 0]  # Adjusted incoming rates
+    time_intervals = [10, 5, 5, 20, 10]  # Adjusted time intervals to cover 45 seconds
+    incoming_rates = [5, 0, 8, 2.5, 0]  # Adjusted incoming rates
     shaping_rate = 4
 
     # Calculate delays over time
